482cb82bb1207702e3112f278e724e8f7e90a711
Mustafa Kum <86945272+Mustafa-Kum@users.noreply.github.com>
2023-01-17 17:30:01 +0300

    Add files via upload

---
+++ ChatGPT-Keyword-Search.py
@@ -0,0 +1,22 @@
+import requests
+
+# List of websites to search
+websites = ['https://infosecwriteups.com/using-chatgpt-to-create-darkweb-monitoring-tool-7b7eeaab351f', 'https://www.another-example.com']
+
+# Keyword to search for
+keyword = 'python'
+
+# Search each website for the keyword
+for website in websites:
+    try:
+        # Send a GET request to the website
+        response = requests.get(website)
+        # Get the response text
+        text = response.text
+        # Check if the keyword is in the response text
+        if keyword in text:
+            print(f'Keyword found on {website}')
+        else:
+            print(f'Keyword not found on {website}')
+    except:
+        print(f'Error accessing {website}')
